460bb1f539c9598b74fbaa430280a285661f6fee
Dakota Junkman <junkmand@os1.engr.oregonstate.edu>
2022-10-14 16:33:00 -0700

    start

---
+++ smallsh.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_INPUT 2048
+#define MAX_ARGS 512
+
+/*
+ * Command structure
+ * Represents a command input by the user
+ * Command will be parsed to fill in the attributes
+ */
+struct command {
+    char* command;
+    char* args[MAX_ARGS];
+    char* redirectFrom;
+    char* redirectTo;
+    bool runInBackground;
+};
+
+
