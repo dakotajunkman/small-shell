a7fdae57398d2961deedcced8923e30ddf754a6e
Dakota Junkman <junkmand@os1.engr.oregonstate.edu>
2022-10-14 18:39:58 -0700

    get input

--- smallsh.c
+++ smallsh.c
@@ -22,3 +22,42 @@ struct command {
     bool runInBackground;
 };
 
+/**
+ * Captures input from terminal and places it into inputArr
+ * Ref: https://stackoverflow.com/questions/45931613/using-getchar-inside-an-infinite-loop-in-c
+ */ 
+void captureCommand(char* inputArr) {
+   int idx = 0;
+   int inputChar;
+   int newline = '\n';
+   int nullterm = '\0';
+    
+    // prompt user for input
+    printf(": ");
+    fflush(stdout);
+    
+    // gather input one character at a time until newline is hit
+   for (;;) {
+       inputChar = getchar();
+
+       // break out when newline is hit or input reaches max
+       if (inputChar == newline || idx == MAX_INPUT - 1) break;
+
+       inputArr[idx++] = inputChar;
+   }
+
+   // terminate this thing
+   inputArr[idx] = nullterm;
+}
+
+int main(void) {
+    char command[MAX_INPUT];
+
+    do {
+        captureCommand(command);
+        printf("command was: %s\n", command);
+    } while (strcmp(command, "exit") != 0);
+
+    return EXIT_SUCCESS;
+}
+
