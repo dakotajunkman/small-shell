18d69e2d82c955347b1c40f7f5cdb50f6d303427
Dakota Junkman <junkmand@os1.engr.oregonstate.edu>
2022-10-20 15:06:40 -0700

    background processes

--- smallsh.c
+++ smallsh.c
@@ -5,9 +5,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define MAX_INPUT 2048
 #define MAX_ARGS 512
+#define MAX_PROCESSES 100
 
 /*
  * Command structure
@@ -21,6 +24,22 @@ struct command {
     char* redirectTo;
     bool runInBackground;
 };
+
+/*
+ * Processes structure
+ * Contains info on currently running background processes
+ * Started background processes will be kept here and checked once per cycle for completion
+ * The array will be circular in nature
+ * curIdx will be modded by MAX_ARGS to stay in bounds and write to the first 0 slot it finds
+ */
+struct processes {
+    pid_t pids[MAX_PROCESSES];
+    int curIdx; // where the next pid should be written in the array
+};
+
+// globals
+int exitStatus = 0;
+bool backGroundAllowed;
 
 /**
  * Captures input from terminal and places it into inputArr
@@ -103,6 +122,36 @@ void copyWithVarExpansion(char* token, char* arg) {
     }
 }
 
+/*
+ * Parses args array to populate input and output files
+ * Searches for redirection symbols and sets appropriate source and destination filepaths
+ * Removes the redirect symbol and file from args array
+ */
+void fillInRedirects(struct command* commandStruct) {
+    int i = 0;
+
+    while (commandStruct->args[i] != NULL) {
+        char* arg = commandStruct->args[i];
+        if (strcmp(arg, "<") == 0) {
+            commandStruct->redirectFrom = malloc(strlen(commandStruct->args[i + 1]) + 1);
+            strcpy(commandStruct->redirectFrom, commandStruct->args[i + 1]);
+
+            // remove redirection from args, otherwise execvp() will try to use them
+            commandStruct->args[i] = '\0';
+            commandStruct->args[i + 1] = '\0';
+            i += 2;
+        } else if (strcmp(arg, ">") == 0) {
+            commandStruct->redirectTo = malloc(strlen(commandStruct->args[i + 1]) + 1);
+            strcpy(commandStruct->redirectTo, commandStruct->args[i + 1]);
+            commandStruct->args[i] = '\0';
+            commandStruct->args[i + 1] = '\0';
+            i += 2;
+        } else {
+            i++;
+        }
+    }
+}
+
 /**
  * parses command input by the user and fills in the command struct
  */ 
@@ -111,19 +160,20 @@ int parseInput(char* command, struct command* commandStruct) {
     // https://stackoverflow.com/questions/58992845/what-is-the-standard-way-to-set-an-array-of-pointers-to-null
     for (size_t i = 0; i < MAX_ARGS; ++i)
         commandStruct->args[i] = NULL;
+    
+    // clean out redirection pointers
+    commandStruct->redirectTo = NULL;
+    commandStruct->redirectFrom = NULL;
 
     int argIdx = 0;
     
     // set up some comparators for common inputs
-    char* newline = "\n";
-    char* comment = "#";
-    char* reDirectOut = ">";
-    char* redirectIn = "<";
+    char comment = '#';
+    char nullterm = '\0';
     char* background = "&";
-    char* pidVar = "$$";
     
     // dip early if the entry was blank or a comment
-    if (command[0] == '\0' || command[0] == '#') return 0;
+    if (command[0] == nullterm || command[0] == comment) return 0;
 
     char* saveptr;
     char* token;
@@ -144,8 +194,32 @@ int parseInput(char* command, struct command* commandStruct) {
         argIdx++;
         token = strtok_r(NULL, " ", &saveptr);
     }
+    
+    // set background process flag
+    commandStruct->runInBackground = (strcmp(commandStruct->args[argIdx - 1], background) == 0);
+
+    // remove & from args if it is there so execvp does not scream
+    if (commandStruct->runInBackground) {
+        free(commandStruct->args[--argIdx]); // deallocate memory
+        commandStruct->args[argIdx] = NULL; // set index to null and decrement argIdx since the argument is gone
+    }
+
+    // check for redirections
+    fillInRedirects(commandStruct);
 
     return argIdx;
+}
+
+/**
+ * Displays the exit status or terminating signal of the most recent foreground process.
+ */
+void displayStatus() {
+    if (exitStatus == 0 || exitStatus == 1)
+        printf("exit value %d\n", exitStatus);
+    else
+        printf("terminated by signal %d\n", exitStatus);
+
+    fflush(stdout);
 }
 
 /**
@@ -158,11 +232,94 @@ void changeDirectory(char* dir, int argCount) {
     chdir(arg);
 }
 
+/*
+ * Sets redirection input and output files if needed
+ * Writes to stderr and exits upon error
+ * Ref: https://canvas.oregonstate.edu/courses/1890465/pages/exploration-system-calls-and-reading-and-writing-files-in-c?module_item_id=22511436
+ */
+void setRedirect(struct command* command) {
+    // create result and file descriptors
+    int result;
+    int sourceFd;
+    int destFd;
+    char* devNull = "/dev/null";
+
+    if (command->redirectFrom) {
+        sourceFd = open(command->redirectFrom, O_RDONLY);
+        if (sourceFd == -1) {
+            fprintf(stderr, "Cannot open file %s\n", command->redirectFrom);
+            exit(1);
+        }
+        result = dup2(sourceFd, STDIN_FILENO);
+        if (result == -1) {
+            perror("Could not redirect stdin\n");
+            exit(1);
+        }
+    } else {
+        // redirection for background processes
+        if (command->runInBackground) {
+            sourceFd = open(devNull, O_RDONLY);
+            if (sourceFd == -1) {
+                perror("Cannot open /dev/null\n");
+                exit(1);
+            }
+
+            result = dup2(sourceFd, STDIN_FILENO);
+            if (result == -1) {
+                perror("Could not redirect stdin\n");
+                exit(1);
+            }
+        }
+    } 
+    if (command->redirectTo) {
+        destFd = open(command->redirectTo, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+        if (destFd == -1) {
+            fprintf(stderr, "Cannot open file %s\n", command->redirectTo);
+            exit(1);
+        }
+        result = dup2(destFd, STDOUT_FILENO);
+        if (result == -1) {
+            perror("Could not redirect stdout\n");
+            exit(1);
+        }
+    } else {
+        // redirection for background processes
+        if (command->runInBackground) {
+            destFd = open(devNull, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+            if (destFd == -1) {
+                perror("Could not open /dev/null\n");
+                exit(1);
+            }
+            result = dup2(destFd, STDOUT_FILENO);
+            if (result == -1) {
+                perror("Could not redirect stdout\n");
+                exit(1);
+            }
+        }
+    }
+}
+
+/*
+ * Writes the pid to the first 0 slot it finds
+ * Uses modulo to create a circular traversal of the array
+ */
+void writePidToProcessArray(struct processes* processes, pid_t pid) {
+    int idx = processes->curIdx;
+
+    // traverse until we hit a 0 slot
+    while (processes->pids[idx] != 0) 
+        idx = (idx + 1) % MAX_ARGS;
+
+    // write in the pid and update curIdx to the new slot
+    processes->pids[idx] = pid;
+    processes->curIdx = ++idx;
+}
+
 /**
  * Creates a new process to run a command
  * Ref: https://canvas.oregonstate.edu/courses/1890465/pages/exploration-process-api-executing-a-new-program?module_item_id=22511470
  */ 
-void runProcess(struct command* command) {
+void runProcess(struct command* command, struct processes* processes) {
     int childStatus;
     pid_t spawnPid = fork();
 
@@ -170,19 +327,34 @@ void runProcess(struct command* command) {
         // error
         case -1:
             perror("fork() failed!\n");
-            exit(EXIT_FAILURE);
+            exit(1);
             break;
         
         // spawned process
         case 0:
+            setRedirect(command);
             execvp(command->command, command->args);
             perror("execvp failed\n");
-            exit(EXIT_FAILURE);
+            exit(1);
             break;
 
         // parent process
         default:
-            spawnPid = waitpid(spawnPid, &childStatus, 0);
+            // when background process, we just need to record the pid and move on
+            if (command->runInBackground) {
+                writePidToProcessArray(processes, spawnPid);
+                printf("background pid is %d\n", spawnPid);
+                fflush(stdout);
+            } else {
+                spawnPid = waitpid(spawnPid, &childStatus, 0);
+
+                // find out if process exited properly
+                // Ref: https://canvas.oregonstate.edu/courses/1890465/pages/exploration-process-api-monitoring-child-processes?module_item_id=22511469
+                if (WIFEXITED(childStatus))
+                    exitStatus = WEXITSTATUS(childStatus);
+                else
+                    exitStatus = WTERMSIG(childStatus);
+            }
             break;
     }
 }
@@ -190,14 +362,16 @@ void runProcess(struct command* command) {
 /**
  * Attempts to execute the entered command
  */ 
-void execCommand(struct command* command, int argCount) {
+void execCommand(struct command* command, struct processes* processes, int argCount) {
     char* cmd = command->command;
     if (strcmp(cmd, "cd") == 0)
         changeDirectory(command->args[1], argCount);
     else if (strcmp(cmd, "exit") == 0)
         return;
+    else if (strcmp(cmd, "status") == 0)
+        displayStatus();
     else 
-        runProcess(command);
+        runProcess(command, processes);
 }
 
 /**
@@ -208,30 +382,69 @@ void freeCommandStruct(struct command* command, int numArgs) {
 
     // Only want to free if memory was allocated
     // Ref: https://stackoverflow.com/questions/2688377/why-exactly-should-i-not-call-free-on-variables-not-allocated-by-malloc
-    /*
-    if (command->redirectTo[0] != '\0')
+    if (command->redirectTo)
         free(command->redirectTo);
 
-    if (command->redirectFrom[0] != '\0')
+    if (command->redirectFrom)
         free(command->redirectFrom);
-    */
 
     for (int i = 0; i < numArgs; ++i)
         free(command->args[i]);
 }
 
+/*
+ * Checks currently running background processes for completion
+ * Prints out status of completed processes
+ * Ref: https://canvas.oregonstate.edu/courses/1890465/pages/exploration-process-api-monitoring-child-processes?module_item_id=22511469
+ */
+void checkBackgroundProcesses(struct processes* processes) {
+    // setup variables that we will need
+    int result;
+    int status;
+
+    for (int i = 0; i < MAX_PROCESSES; ++i) {
+        pid_t pid = processes->pids[i];
+
+        // we need to check it for completion
+        if (pid != 0) {
+            result = waitpid(pid, &status, WNOHANG);
+            
+            // process has completed, we need to display the status
+            if (result != 0) {
+                
+                // exited normally
+                if (WIFEXITED(status))
+                    printf("background pid %d is done: exit value %d\n", pid, WEXITSTATUS(status));
+                else
+                    printf("background pid %d is done: terminated by signal %d\n", pid, WTERMSIG(status));
+
+                fflush(stdout);
+                processes->pids[i] = 0; // frees up space to be used again now that process is done
+            }
+        }
+    }
+}
+
 int main(void) {
     char command[MAX_INPUT];
     struct command commandStruct;
-    commandStruct.runInBackground = false; // default value
+    struct processes processes;
+    
+    // clean out the pid memory space
+    for (int i = 0; i < MAX_PROCESSES; ++i)
+        processes.pids[i] = 0;
+
+    processes.curIdx = 0;
+    backGroundAllowed = true; // default here to start
 
     do {
         captureCommand(command);
         int commandArgs = parseInput(command, &commandStruct);
         if (commandArgs) {
-            execCommand(&commandStruct, commandArgs);
+            execCommand(&commandStruct, &processes, commandArgs);
             freeCommandStruct(&commandStruct, commandArgs);
         }
+        checkBackgroundProcesses(&processes);
     } while (strcmp(command, "exit") != 0);
 
     return EXIT_SUCCESS;
